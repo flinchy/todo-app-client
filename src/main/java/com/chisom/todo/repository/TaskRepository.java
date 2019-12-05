ecbf7cb97af7b64ba332438f08685ed6d5dd97d6
Chisom <iwowochisom@gmail.com>
2019-12-05 15:54:40 +0100

    Modified TaskRepository

--- src/main/java/com/chisom/todo/repository/TaskRepository.java
+++ src/main/java/com/chisom/todo/repository/TaskRepository.java
@@ -2,10 +2,7 @@ package com.chisom.todo.repository;
 
 import com.chisom.todo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
