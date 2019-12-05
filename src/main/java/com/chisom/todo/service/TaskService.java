33c4ecff241df15567d9178c8f6a20669e4a3709
Chisom <iwowochisom@gmail.com>
2019-12-05 09:30:35 +0100

    Create TaskService interface that includes retrieveAllTasks, retrieveTaskById, and createNewTask methods

---
+++ src/main/java/com/chisom/todo/service/TaskService.java
@@ -0,0 +1,12 @@
+package com.chisom.todo.service;
+
+import com.chisom.todo.entity.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    List<Task> retrieveAllTasks();
+    Task retrieveTaskById(Long id);
+    Task createNewTask(Task task);
+}
