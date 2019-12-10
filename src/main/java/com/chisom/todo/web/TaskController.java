7bd955a79d3d99a2997addc08856c901f912d8f2
Chisom <iwowochisom@gmail.com>
2019-12-10 22:47:28 +0100

    Modify TaskController

--- src/main/java/com/chisom/todo/web/TaskController.java
+++ src/main/java/com/chisom/todo/web/TaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://todo-flinchy.netlify.com/")
 @RestController
 public class TaskController {
 
