c8fb40be839112169fc680a7cc4e33b89f77846a
Chisom <iwowochisom@gmail.com>
2019-12-11 13:59:56 +0100

    preserves createdAt field in the database after an update
    closes #1

--- src/main/java/com/chisom/todo/web/TaskController.java
+++ src/main/java/com/chisom/todo/web/TaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://todo-flinchy.netlify.com")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TaskController {
 
